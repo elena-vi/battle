98d57afd170af1908f27bcc9d0e202df246d1af2
LukaszGr <lukasz.grychowski@gmail.com>
2016-06-08 15:46:31 +0100

    logic extracted

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class App < Sinatra::Base
   enable :sessions
@@ -8,20 +9,22 @@ class App < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    session[:player_1] = Player.new(params[:player_1])
+    session[:player_2] = Player.new(params[:player_2])
     @@status = []
     redirect('/play')
   end
 
   get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
+    @player_1_hp = session[:player_1].hp
+    @player_1_name = session[:player_1].name
+    @player_2_hp = session[:player_2].hp
+    @player_2_name = session[:player_2].name
     @game_status = @@status
     erb(:game)
   end
 
-  post '/attack' do 
+  post '/attack' do
     @@status << "#{params[:target]} got attacked"
     # p @status
     redirect('/play')
---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+
+  attr_reader :name, :hp
+
+  def initialize(name)
+    @name = name
+    @hp = 100
+  end
+end
