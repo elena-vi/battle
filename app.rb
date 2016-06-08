32d1761d7dc8e76cfe2688f6fd72d3e05816e0af
LukaszGr <lukasz.grychowski@gmail.com>
2016-06-08 09:50:58 +0100

    few changes to /names

--- app.rb
+++ app.rb
@@ -1,17 +1,18 @@
 require 'sinatra/base'
-require 'shotgun'
 
 class App < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb(:names)
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    erb(:game)
   end
+
 
   run! if app_file == $0
 end
