6ab3b3915659306039f806769dc653860873fdc6
Elena <elena.vilimaite@gmail.com>
2016-06-08 12:04:31 +0100

    clean up and web helpers

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ class App < Sinatra::Base
   post '/names' do
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:game)
   end
 
--- spec/features/create_name_spec.rb
+++ spec/features/create_name_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe 'App' do
-  it 'allows players to enter their names' do
-    visit('/')
-    fill_in :player_1, with: 'Hassan'
-    fill_in :player_2, with: 'Lukasz'
-    click_button 'Submit'
-    expect(page).to have_content 'Hassan vs. Lukasz'
-  end
+	it 'allows players to enter their names' do
+		sign_in_and_play
+		expect(page).to have_content 'Hassan vs. Lukasz'
+	end
+	it "allows palyers to see hp" do
+		sign_in_and_play
+		expect(page).to have_content '100HP'
+	end
 end
