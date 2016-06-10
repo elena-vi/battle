ec65b81570c331f37cf4aed0b234d7bee36bb0f1
Elena <elena.vilimaite@gmail.com>
2016-06-10 11:05:49 +0100

    bla

--- lib/game.rb
+++ lib/game.rb
@@ -3,9 +3,19 @@ class Game
 	def initialize player1, player2
 		@player_1 = player1
 		@player_2 = player2
+		@current = @player_1
 	end
 
 	def attack player
 		player.attacked
 	end
+
+	def switch
+		if @current_player == @playe_r1
+			@current_player = @player_2.enable
+		else
+			@current_player = @player_1.enable
+		end
+	end
+
 end
--- lib/player.rb
+++ lib/player.rb
@@ -10,4 +10,22 @@ class Player
   def attacked
     @hp -= 10
   end
+
+  def enabled
+  end 
 end
+
+
+
+
+
+
+# if $game.current_player == $game.player1
+#       @disable_player2 = "disabled"
+#       @disable_player1 = ""
+#       @current_player = @player_1_name
+#     else
+#       @disable_player1 = "disabled"
+#       @disable_player2 = ""
+#       @current_player = @player_2_name
+# end
